7cec8cc233169f364ed5f3185a8cfffe6d4bf5e9
DanBeaumont <daniel.beaumont@aie.edu.au>
2020-04-29 17:33:20 +0930

    Added basic level design and weapon
    - implemented basic level design (not finished)
    - Added rocket launcher
    - added textures
    - added materials
    - added some lighting

---
+++ fpscasestudy/Assets/Scripts/RocketAmmoPickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketAmmoPickup : MonoBehaviour
+{
+    public int amount = 5;                              // how many rockets this pickup is worth
+    public float rotationSpeed = 20f;                   // how fast the ammo box rotates
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, transform.up, rotationSpeed * Time.deltaTime);       // rotate the ammo box
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RocketLauncher rocketLauncher = other.gameObject.GetComponentInChildren<RocketLauncher>();      // checks to see if we have a rocketlauncher attached to the object
+
+        if (rocketLauncher != null)                                             // if we found a rocketlauncher
+        {
+            rocketLauncher.ammo += amount;                                      // add some ammo to that rocketlauncher
+            gameObject.SetActive(false);                                        // ...and deactivate ourselves
+        }
+
+    }
+}
---
+++ fpscasestudy/Assets/Scripts/RocketLauncher.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour
+{
+    public bool playerControlled = true;    // Is this RocketLauncher controlled by the player?
+    public float fireInterval = 3.0f;       // how many seconds between shots for this rocket launcher?
+    
+    private float fireTimer = 0;            // keep track of when we can fire again
+
+    public GameObject rocketPrefab;         // reference to Rocket prefab so we spawn it
+    public Vector3 spawnOffset;             // a position offset for where the rocket is spawned so that we dont spawn inside the gun
+
+    public int ammo = 5;                    // how many rockets the player has
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireTimer = fireInterval;           // when the game starts we are ready to shoot
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireTimer += Time.deltaTime;                                            // advance the timer
+
+        if (fireTimer >= fireInterval)                                          // have we waited long enough to shoot?
+        {
+            if (playerControlled && Input.GetButtonDown("Fire1"))               // is rocket player controlled and fire button pressed?
+            {
+                Fire();                                                         // Fire a rocket
+            }
+        }
+    }
+
+    public void Fire()
+    {
+        if (ammo <= 0)                                                          // only shoot if there is ammo
+        {
+            return;
+        }
+
+        GameObject rocketInstance = Instantiate(rocketPrefab);                  // spawn and store a new rocket prefab
+
+        // apply the spawn offset relative to the gun position
+        rocketInstance.transform.position = transform.position + 
+            transform.right * spawnOffset.x +
+            transform.up * spawnOffset.y +
+            transform.forward * spawnOffset.z;
+
+        rocketInstance.transform.rotation = transform.rotation;                 // rotate the rocket to match the guns rotation
+
+        fireTimer = 0;                                                          // reset fire timer
+        ammo -= 1;                                                              // reduce ammo count
+
+    }
+}
